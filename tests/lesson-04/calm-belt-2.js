435f86226e63df065ac34ccc3754eaa6fc2244b1
= <ngocvuong95@gmail.com>
2026-09-23 02:58:00 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/calm-belt-2.js
@@ -0,0 +1,18 @@
+let devilFruit = "gomu-gomu";
+let effect = "";
+if (devilFruit === "gomu-gomu") {
+  effect = "Gear 5 Unlocked";
+  console.log(effect);
+} else if (devilFruit === "mera-mera") {    
+    effect = "Flame Emperor"
+  console.log(effect);
+} else if (devilFruit === "ope-ope") {
+    effect = "Room & Ope";
+  console.log(effect);
+} else if (devilFruit === "none") {
+    effect = "Normal Human"
+  console.log(effect);
+} else {
+    effect= "Unknown Devil Fruit"
+  console.log(effect);
+}
