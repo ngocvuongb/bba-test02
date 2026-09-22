4c54b3839e6a1c2f0135602bac93411c5d6f7a28
= <ngocvuong95@gmail.com>
2026-09-23 02:56:30 +0700

    feat: add solution for calm belt challenge

---
+++ tests/lesson-04/calm-belt.js
@@ -0,0 +1,14 @@
+let navigatorName = "Luffy";
+let currentHakiLevel = 3;
+const islandA = 15;
+const islandB = 25;
+const islandC = 40;
+
+let totalMeat = islandA + islandB + islandC;
+let averageMeat = totalMeat/3;
+
+console.log(totalMeat);
+console.log(averageMeat);
+console.log(`Lượng thịt dư sau khi chia đều cho 3 thành viên: ${totalMeat%3}`);
+
+ 
